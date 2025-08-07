b96c51c2c65582f1b9384c2a086276cd880ad34c
Levente Proksa <proksalevente@gmail.com>
2025-08-07 09:08:10 +0200

    rem: unused code

--- public/admin.js
+++ public/admin.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $('.edit-btn').click(function() {
+$(document).ready(function () {
+    $('.edit-btn').click(function () {
         const id = $(this).data('id');
         const title = $(this).data('title');
         const content = $(this).data('content');
@@ -10,15 +10,6 @@ $(document).ready(function() {
 
         $('#submit-button').text('Save');
     });
-
-    // $('#news-form').on('submit', function(e) {
-    //     setTimeout(() => {
-    //         $('#news-id').val('');
-    //         $('#title').val('');
-    //         $('#content').val('');
-    //         $('#submit-button').text('Create');
-    //     }, 500);
-    // });
 
     $('#logout-button').click(function () {
         window.location.href = '/?action=logout';
